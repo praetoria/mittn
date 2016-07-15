5898e0101575a4ebc5ee100f6697ce1097187c8c
juhakivekas <juha.kivekas@helnisnki.fi>
2016-07-15 13:06:06 +0300

    modify tests to pass :D

--- fuzzer/mittn/fuzzer/test/test_archiver.py
+++ fuzzer/mittn/fuzzer/test/test_archiver.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 import unittest
 import uuid
+import copy
 
 from sqlalchemy.orm.session import Session
 from mittn.fuzzer.archiver import Archiver
@@ -15,18 +16,18 @@ class test_archiver(unittest.TestCase):
         self.db_file = os.path.join(tempfile.gettempdir(), 'mittn_unittest.' + str(uuid.uuid4()))
         self.db_url = 'sqlite:///' + self.db_file
 
-    def dburl_not_defined(self):
+    def test_dburl_not_defined(self):
         a = Archiver()
         a.init()
         assert a.session is None, "No db_url provided, should return None as connection"
 
-    def create_db_connection(self):
+    def test_create_db_connection(self):
         # Try whether an actual database connection can be opened
         a = Archiver(self.db_url)
         a.init()
         assert isinstance(a.session, Session), "An SQLAlchemy connection object was not returned"
 
-    def number_of_new_false_positives(self):
+    def test_number_of_new_false_positives(self):
         # Add a couple of false positives to database as new issues,
         # and check that the they're counted properly
         a = Archiver(self.db_url)
@@ -34,93 +35,96 @@ class test_archiver(unittest.TestCase):
 
         # OK: Add one, expect count to be 1
         issue = Issue(
-            issue_no=1,
-            new_issue=True,
-            timestamp=datetime.datetime(2016,1,1),
-            test_runner_host='testhost',
-            scenario_id='test-scenario',
-            url='test.io',
-            server_timeout=False,
-            server_error_text_detected=False
+            issue_no = 1,
+            new_issue = True,
+            timestamp = datetime.datetime(2016,1,1),
+            test_runner_host = 'testhost',
+            scenario_id = 'test-scenario',
+            url = 'test.io',
+            server_timeout = False,
+            server_error_text_detected = False
         )
         a.add_issue(issue)
         assert a.new_issue_count() == 1
-        """
+
         # OK: Add a second one, expect count to be 2
         issue = Issue(
-            issue_no=2,
-            new_issue=True,
-            timestamp=datetime.datetime(2016,1,1),
-            test_runner_host='testhost',
-            scenario_id='test-scenario',
-            url='test.io',
-            server_timeout=False,
-            server_error_text_detected=False
+            issue_no = 2,
+            new_issue = True,
+            timestamp = datetime.datetime(2016,1,1),
+            test_runner_host = 'testhost',
+            scenario_id = 'test-scenario',
+            url = 'test.io',
+            server_timeout = False,
+            server_error_text_detected = False
         )
         a.add_issue(issue)
         assert a.new_issue_count() == 2
-        """
-    def false_positive_detection(self):
+
+    def test_false_positive_detection(self):
         # Test whether false positives in database are identified properly
-        response = {'scenario_id': '1',
-                    'req_headers': 'headers',
-                    'req_body': 'body',
-                    'url': 'url',
-                    'req_method': 'method',
-                    'timestamp': datetime.datetime.utcnow(),
-                    'server_protocol_error': False,
-                    'server_timeout': False,
-                    'server_error_text_detected': False,
-                    'server_error_text_matched': 'matched_text',
-                    'resp_statuscode': 'statuscode',
-                    'resp_headers': 'resp_headers',
-                    'resp_body': 'resp_body',
-                    'resp_history': 'resp_history'}
+        response = Issue(
+            scenario_id = 1,
+            req_headers = b'headers',
+            req_body = b'body',
+            url = 'url',
+            req_method = 'method',
+            timestamp = datetime.datetime.utcnow(),
+            test_runner_host = 'testhost',
+            server_protocol_error = False,
+            server_timeout = False,
+            server_error_text_detected = False,
+            server_error_text_matched = 'matched_text',
+            resp_statuscode = 'statuscode',
+            resp_headers = b'resp_headers',
+            resp_body = b'resp_body',
+            resp_history = b'resp_history'
+        )
 
         # First add one false positive and try checking against it
-        dbtools.add_issue(self.context, response)
+        a = Archiver(self.db_url)
+        a.init()
+        a.add_issue(response)
 
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        self.assertEqual(a.known_false_positive(response),
                          True, "Duplicate false positive not detected")
 
         # Change one of the differentiating fields, and test, and
         # add the tested one to the database.
-        response['scenario_id'] = '2'  # Non-duplicate
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        response = copy.deepcopy(response)
+        response.scenario_id = 2 # Non-duplicate
+        self.assertEqual(a.known_false_positive(response),
                          False, "Not a duplicate: scenario_id different")
-        dbtools.add_issue(self.context, response)
+        a.add_issue(response)
 
         # Repeat for all the differentiating fields
-        response['server_protocol_error'] = 'Error text'
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        response = copy.deepcopy(response)
+        response.server_protocol_error = 'Error text'
+        self.assertEqual(a.known_false_positive(response),
                          False, "Not a duplicate: server_protocol_error different")
-        dbtools.add_issue(self.context, response)
+        a.add_issue(response)
 
-        response['resp_statuscode'] = '500'
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        response = copy.deepcopy(response)
+        response.resp_statuscode = '500'
+        self.assertEqual(a.known_false_positive(response),
                          False, "Not a duplicate: resp_statuscode different")
-        dbtools.add_issue(self.context, response)
+        a.add_issue(response)
 
-        response['server_timeout'] = True
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        response = copy.deepcopy(response)
+        response.server_timeout = True
+        self.assertEqual(a.known_false_positive(response),
                          False, "Not a duplicate: server_timeout different")
-        dbtools.add_issue(self.context, response)
+        a.add_issue(response)
 
-        response['server_error_text_detected'] = True
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        response = copy.deepcopy(response)
+        response.server_error_text_detected = True
+        self.assertEqual(a.known_false_positive(response),
                          False, "Not a duplicate: server_error_text_detected different")
-        dbtools.add_issue(self.context, response)
+        a.add_issue(response)
 
         # Finally, test the last one again twice, now it ought to be
         # reported back as a duplicate
-        self.assertEqual(dbtools.known_false_positive(self.context,
-                                                      response),
+        self.assertEqual(a.known_false_positive(response),
                          True, "A duplicate case not detected")
 
     def tearDown(self):
@@ -128,3 +132,6 @@ class test_archiver(unittest.TestCase):
             os.unlink(self.db_file)
         except:
             pass
+
+if __name__ == '__main__':
+    unittest.main()
