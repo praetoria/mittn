68924856993d79881c18c08242a1ae72ca42d1e7
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-07-15 10:31:13 +0300

    added some comments and moved checker into its own file

--- tls/mittn/test.py
+++ tls/mittn/test.py
@@ -1,8 +1,10 @@
 from tls.steps import MittnTLSChecker, Config
 
 c = Config('mittn.conf')
+
 # don't check for preferred or enabled cipher suites
 c.suites_preferred = []
 c.suites_enabled = []
+
 t = MittnTLSChecker(config=c)
 t.run('www.google.com')
