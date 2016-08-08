0643e0490cb54a79b93935c8df860093a574bbcf
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-08 14:00:54 +0300

    refactored fuzzerdefaults.py to make more sense

--- mittn/fuzzer/__init__.py
+++ mittn/fuzzer/__init__.py
@@ -1,3 +1,3 @@
 from mittn  import Config
-from mittn.fuzzer.fuzzerdefaults import FuzzerDefaultConfig
-Config.tools["fuzzer"] = FuzzerDefaultConfig()
+from mittn.fuzzer.fuzzerdefaults import FuzzerDefaults
+Config.tools["fuzzer"] = FuzzerDefaults()
--- mittn/fuzzer/fuzzerdefaults.py
+++ mittn/fuzzer/fuzzerdefaults.py
@@ -1,12 +1,18 @@
-class FuzzerDefaultConfig(object):
+class FuzzerDefaults(object):
     def __init__(self):
         # attributes in the config which are lists
         self.list_attributes = [
                 "methods",
+                "allowed_statuses",
+                "disallowed_statuses",
                 ]
         self.defaults = """
 [fuzzer]
 methods=GET,POST
 radamsa_path=/usr/bin/radamsa
 timeout=30
-        """
+allowed_statuses=
+    200,404
+disallowed_statuses=
+anomalies=1
+"""
