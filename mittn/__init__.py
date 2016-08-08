a2842d9602eba3e5132e01615e6fc83f5983ebf2
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-08 14:09:12 +0300

    added Archiver import into root __init__.py

--- mittn/__init__.py
+++ mittn/__init__.py
@@ -1,6 +1,7 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 from .config import Config
+from .archiver import Archiver
 from .tls.mittntlschecker import MittnTlsChecker, TlsChecker
 from .fuzzer.mittnfuzzer import MittnFuzzer
 from .fuzzer.target import Target
