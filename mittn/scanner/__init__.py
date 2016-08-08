32b4832eb1e78b75fae1a987d13fd998d449f45e
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-08 14:03:48 +0300

    started writing the scanner plugin

---
+++ mittn/scanner/__init__.py
@@ -0,0 +1,3 @@
+from mittn  import Config
+from mittn.scanner.scannerdefaults import ScannerDefaults
+Config.tools["scanner"] = ScannerDefaults()
---
+++ mittn/scanner/scannerdefaults.py
@@ -0,0 +1,8 @@
+class ScannerDefaults(object):
+    def __init__(self):
+        # attributes in the config which are lists
+        self.list_attributes = [
+                ]
+        self.defaults = """
+[scanner]
+        """
