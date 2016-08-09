acc47b9da858eed6e0dd4035d3110c49a1e55931
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-09 13:13:07 +0300

    added a run_tests method to MittnScanner class

--- mittn/scanner/mittnscanner.py
+++ mittn/scanner/mittnscanner.py
@@ -1,6 +1,6 @@
 from mittn.config import Config
 from mittn.archiver import Archiver
-from mittn.scanner.pythonburp import PythonBurp
+#from mittn.scanner.pythonburp import PythonBurp
 
 class MittnScanner(object):
     def __init__(self,archiver=None,burp=None,config=None):
@@ -9,4 +9,21 @@ class MittnScanner(object):
         if hasattr(self.config,'db_url'):
             db_url = self.config.db_url
         self.archiver = archiver or Archiver(db_url)
-        burp = burp or PythonBurp(self.config.burp_cmdline)
+        #burp = burp or PythonBurp(self.config.burp_cmdline)
+        self.results = []
+    
+    def run_tests(self,testfunction,tests):
+        """ Takes a test function which takes a test name
+        as a parameter and a list of test names to feed it.
+        """
+        # TODO: This is just a preliminary draft.
+        for test in tests:
+            if testfunction(test) != 0:
+                raise RuntimeError("Test '%s' failed to execute" % test)
+            #burp.finish()
+            #result = burp.collect()
+            #self.results.append(result)
+
+    def collect_results(self):
+        # TODO: Return a count of new findings etc
+        return len(self.results)
