1ebcb7c475f63d634e9bc85a93e4e78e8622efa7
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-09 09:28:49 +0300

    fixed the config section name for mittnscanner

--- mittn/scanner/mittnscanner.py
+++ mittn/scanner/mittnscanner.py
@@ -4,7 +4,7 @@ from mittn.scanner.pythonburp import PythonBurp
 
 class MittnScanner(object):
     def __init__(self,archiver=None,burp=None,config=None):
-        self.config = config or Config("fuzzer","mittn.conf")
+        self.config = config or Config("scanner","mittn.conf")
         db_url = None
         if hasattr(self.config,'db_url'):
             db_url = self.config.db_url
