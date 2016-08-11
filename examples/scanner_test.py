c9e6d2e9b43974dcc8a4ef3ecb1cbb4e806561cd
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-11 16:35:57 +0300

    fixed an example file

--- examples/scanner_test.py
+++ examples/scanner_test.py
@@ -3,8 +3,8 @@ from mittn import Archiver
 from subprocess import call
 
 def testfunction(test,proxy):
-    status = call('curl http://localhost:9000/%s?input=aoeu' +
-        ' --proxy http://%s 1>/dev/null 2>/dev/null' % (test,proxy),
+    status = call(('curl http://localhost:9000/%s?input=aoeu' +
+        ' --proxy http://%s 1>/dev/null 2>/dev/null') % (test,proxy),
             shell=True)
     # True if succeeded, False otherwise
     return status == 0
