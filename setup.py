eb12add8de062d86693bd2a2d44a4dffe8a6b69e
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-12 12:03:38 +0300

    setup.py works with pypi now

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup,find_packages
 
 setup(
     name = 'mittn',
-    packages = ['mittn'],
-    version = '0.03',
+    packages = find_packages(),
+    version = '0.3.0',
     description='Mittn security test automation',
     classifiers=[
           "Programming Language :: Python :: 3.4"
@@ -12,7 +12,10 @@ setup(
     author='F-Secure Corporation',
     author_email='opensource@f-secure.com',
     url='https://github.com/praetoria/mittn',
-    download_url='https://github.com/praetoria/mittn/tarball/0.03',
-    keywords = ['fuzz','fuzzing','security','test','scanner']
-    install_requires=open('requirements.txt').readlines(),
+    download_url='https://github.com/praetoria/mittn/tarball/0.3.0',
+    keywords = ['fuzz','fuzzing','security','test','scanner'],
+    install_requires=['pytz',
+        'python-dateutil',
+        'sqlalchemy',
+        'requests']
 )
