337e18a92a6779f98529b3f632cbf3b5897f7dd9
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-12 16:13:02 +0300

    updated defaults to the right option names in scannerdefaults

--- mittn/scanner/scannerdefaults.py
+++ mittn/scanner/scannerdefaults.py
@@ -1,11 +1,10 @@
 class ScannerDefaults(object):
     def __init__(self):
         # attributes in the config which are lists
-        self.list_attributes = [
-                ]
+        self.list_attributes = []
         self.defaults = """
 [scanner]
-burp_cmdline=java -jar -Xmx1g -Djava.awt.headless=true -XX:MaxPermSize=1G
-burp_proxy_address=127.0.0.1:8080
+cmdline=java -jar -Xmx1g -Djava.awt.headless=true -XX:MaxPermSize=1G
+proxy_address=127.0.0.1:8080
 timeout=1
 """
