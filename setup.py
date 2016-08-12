ba0877f36cf558f12bde812a96f16b406aef805b
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-12 10:26:32 +0300

    configured setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name='mittn',
-    use_scm_version={'root': '..'},
+    name = 'mittn',
+    packages = ['mittn'],
+    version = '0.03',
     description='Mittn security test automation',
-    long_description='',
     classifiers=[
           "Programming Language :: Python :: 3.4"
     ],
     license='Apache License 2.0',
     author='F-Secure Corporation',
     author_email='opensource@f-secure.com',
-    url='https://github.com/F-Secure/mittn',
-    packages=find_packages(),
+    url='https://github.com/praetoria/mittn',
+    download_url='https://github.com/praetoria/mittn/tarball/0.03',
+    keywords = ['fuzz','fuzzing','security','test','scanner']
     install_requires=open('requirements.txt').readlines(),
 )
