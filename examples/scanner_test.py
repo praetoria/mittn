1b5aa46ad485866c07f069108ec1b2d3a96d88f7
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2016-08-15 13:28:57 +0300

    improved the examples

--- examples/scanner_test.py
+++ examples/scanner_test.py
@@ -14,10 +14,31 @@ tests = [
         'xss2',
         ]
 
+# creating a config object is optional
+# if it is not created a default config
+# will be created and mittn.conf will
+# be read from the current directory
+c = Config('scanner')
+
+# burp proxy address
+c.proxy_address = '127.0.0.1:8080'
+
+# timeout in minutes for the tests
+c.timeout = '1'
+
+# creating the archiver is also optional
+# but if it is not created db_url has to
+# be specified in the config file or object
 a = Archiver('sqlite:////tmp/db')
-scanner = MittnScanner(archiver=a)
-scanner.init() # initialize db
+
+scanner = MittnScanner(archiver=a,config=c)
+
+# initialize the database
+scanner.init()
 scanner.run_tests(testfunction,tests)
 results = scanner.collect_results()
+
+# unless there are any findings results
+# are an empty list
 for r in results:
     print(r)
