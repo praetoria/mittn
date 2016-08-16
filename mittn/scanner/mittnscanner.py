0ad9fdb51c450f7535ae17210b9bd7ede20459a1
Juha Kivekäs <juha.kivekas@helsinki.fi>
2016-08-16 10:49:08 +0300

    fixed indentation, default config behaviour, and a function name

--- mittn/scanner/mittnscanner.py
+++ mittn/scanner/mittnscanner.py
@@ -5,7 +5,7 @@ from mittn.scanner.scannerissue import ScannerIssue
 
 class MittnScanner(object):
     def __init__(self,archiver=None,scanner=None,config=None):
-        self.config = config or Config("scanner","mittn.conf")
+        self.config = config or Config("scanner", None)
         db_url = None
         if hasattr(self.config,'db_url'):
             db_url = self.config.db_url
@@ -26,17 +26,17 @@ class MittnScanner(object):
             self.scanner.start()
             for test in tests:
                 if not testfunction(test,self.config.proxy_address):
-                    raise RuntimeError("Valid test scenario '%s'" +
-                            " failed to execute, using proxy %s" % (test,
-                                self.config.proxy_address))
+                    raise RuntimeError(
+                        "Valid test scenario '%s' failed to execute, using proxy %s"
+                        % (test, self.config.proxy_address))
                 self.scanner.finish(int(self.config.timeout))
                 result = self.scanner.collect()
                 for r in result:
                     issue = ScannerIssue.issue_from_dict(test,r)
                     self.archiver.add_if_not_found(issue)
-                self.results.append(result)
+                    self.results.append(r)
         finally:
             self.scanner.kill()
 
-    def collect_results(self):
+    def get_results(self):
         return self.results
--- mittn/scanner/scannerdefaults.py
+++ mittn/scanner/scannerdefaults.py
@@ -5,7 +5,9 @@ class ScannerDefaults(object):
                 ]
         self.defaults = """
 [scanner]
-burp_cmdline=java -jar -Xmx1g -Djava.awt.headless=true -XX:MaxPermSize=1G
-burp_proxy_address=127.0.0.1:8080
+db_url=sqlite:////tmp/mittn_scanner_issues.db
+path=/home/kiveju2/burp/burpsuite_pro_v1.7.04.jar
+cmdline=java -jar -Xmx1g -Djava.awt.headless=true -XX:MaxPermSize=1G
+proxy_address=127.0.0.1:8080
 timeout=1
 """
